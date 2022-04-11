10277fd7fc01a25dccd31ca566fb337a88d41040
DAZ <daz4126@gmail.com>
2022-04-11 16:01:18 +0100

    Initial commit

---
+++ statement.js
@@ -0,0 +1,24 @@
+import {html, svg, render} from 'html';
+
+function Statement({state,element=document.body,template}={}) {
+  render(element,template(state));
+  const handler = {
+    set(obj,key,value) {
+      if (typeof obj[key] === 'object' && !obj[key]._isProxy) {
+        obj[key] = new Proxy(obj[key],handler);
+      }
+      obj[key] = typeof value === "function" ? value(state) : value;
+      render(element,template(state));
+      return Reflect.set(...arguments);
+    }    
+  };
+  const statement = new Proxy(state,handler);
+  Object.entries(statement).forEach(([key,value]) => {
+    if(typeof value === "object") {
+      statement[key] = new Proxy(statement[key],handler);
+    }
+  });
+  return statement;
+}
+
+export {html, svg, Statement}
